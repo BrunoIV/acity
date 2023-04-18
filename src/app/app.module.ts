0dc0f194b154e02acd56f71fa7e97a0ec6d54752
Bruno Expósito <bexposito@edicom.es>
2023-04-18 19:41:52 +0200

    muevo bottom-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BottomBarComponent } from './components/ui/bottom-bar/bottom-bar.component';
+import { BottomBarComponent } from './components/ui/bottom-bar/bottom-bar/bottom-bar.component';
 import { GameComponent } from './components/layers/game/game.component';
 
 @NgModule({
