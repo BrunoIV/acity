37e9d1c5ef0ccd5b4967452cc46fcd4a35e8ad2a
Bruno Expósito <bexposito@edicom.es>
2023-04-18 19:18:35 +0200

    Agrego BottomBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BottomBarComponent } from './components/ui/bottom-bar/bottom-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomBarComponent
   ],
   imports: [
     BrowserModule,
