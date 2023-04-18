86b082ff994a31e01af56a000f2ce5aefd1bc3dc
Bruno Expósito <bexposito@edicom.es>
2023-04-18 19:21:51 +0200

    Agrego la capa Game y coloco BottomBar en su lugar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BottomBarComponent } from './components/ui/bottom-bar/bottom-bar.component';
+import { GameComponent } from './components/layers/game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BottomBarComponent
+    BottomBarComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
