923bb8b614e14aa343a34a18ad09873058df6c02
Bruno Expósito <bexposito@edicom.es>
2023-04-23 16:29:17 +0200

    Primer edificio: Casa (vacío)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { GameComponent } from './components/layers/game/game.component';
 import { ZoneDemandComponent } from './components/ui/bottom-bar/zone-demand/zone-demand.component';
 import { DateControlComponent } from './components/ui/bottom-bar/date-control/date-control.component';
 import { ConstructionMenuComponent } from './components/ui/bottom-bar/construction-menu/construction-menu.component';
+import { HouseComponent } from './components/buildings/house/house.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ConstructionMenuComponent } from './components/ui/bottom-bar/constructi
     GameComponent,
     ZoneDemandComponent,
     DateControlComponent,
-    ConstructionMenuComponent
+    ConstructionMenuComponent,
+    HouseComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/buildings/house/house.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'building-house',
+  templateUrl: './house.component.html',
+  styleUrls: ['./house.component.css']
+})
+export class HouseComponent {
+
+}
