fa492798f739116937e344ea07e0ae22fc31073a
Bruno Expósito <bexposito@edicom.es>
2023-04-24 06:29:34 +0200

    Agrego a House parámetro "debug" para mostrar un texto dentro

--- src/app/components/buildings/house/house.component.ts
+++ src/app/components/buildings/house/house.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'building-house',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./house.component.css']
 })
 export class HouseComponent {
+  @Input() debug = '';
 
 }
